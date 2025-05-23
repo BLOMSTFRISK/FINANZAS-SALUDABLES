ee3ba91fb2129bad8769537aa0777a75138bb387
BLOMSTFRISK <blomstfrisk@gmail.com>
2025-05-23 10:26:22 -0300

    v1

--- originales/auth.js
+++
@@ -1,7 +0,0 @@
-
-// auth.js - Protección de páginas con login
-
-if (!sessionStorage.getItem('usuarioLogueado')) {
-  sessionStorage.setItem('urlDestino', window.location.href);
-  window.location.href = 'login.html';
-}
